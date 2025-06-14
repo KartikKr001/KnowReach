e3dc819ba16f527caf3e4da61caa94c7f424f807
Kartik Kumar <kartikkrprajapati01@gmail.com>
2025-06-14 14:10:04 +0530

    Basic Route Setup Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
-const Page = () => {
+const Page = () =>{
   return (
     <div>
       <h1 className='text-2xl underline'>Hello</h1>
