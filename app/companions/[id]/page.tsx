43f7f5f01ee681f7cb9d6c20c48e4b60fca2db76
Kartik Kumar <kartikkrprajapati01@gmail.com>
2025-06-22 23:46:04 +0530

    Compnanion components etup done enabled talking with assistant

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -4,7 +4,7 @@ import {redirect} from "next/navigation";
 import {getSubjectColor} from "@/lib/utils";
 import Image from "next/image";
 import { getCompanion } from "@/lib/actions/companions.actions";
-// import CompanionComponent from "@/components/CompanionComponent";
+import CompanionComponent from "@/components/CompanionComponent";
 
 interface CompanionSessionPageProps {
     params: Promise<{ id: string}>;
@@ -20,39 +20,39 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
     if(!user) redirect('/sign-in');
     const { name, subject, topic, duration } = companion;
     if(!name) redirect('/companions')
-    return (
-        <main>
-            <article className="flex rounded-border justify-between p-6 max-md:flex-col">
-                <div className="flex items-center gap-2">
-                    <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={{                 background: `linear-gradient(to right, ${getSubjectColor(subject)[0]}, ${getSubjectColor(subject)[1]})` }}>
-                        <Image src={`/icons/${subject}.svg`} alt={subject} width={35} height={35} />
-                    </div>
+ return (
+    <main>
+      <article className="flex rounded-border justify-between p-4 max-md:flex-col max-md:gap-2 max-h-[120px] overflow-hidden">
+        <div className="flex items-center gap-3">
+          <div
+            className="size-[48px] flex items-center justify-center rounded-md max-md:hidden"
+            style={{
+              background: `linear-gradient(to right, ${getSubjectColor(subject)[0]}, ${getSubjectColor(subject)[1]})`,
+            }}
+          >
+            <Image src={`/icons/${subject}.svg`} alt={subject} width={24} height={24} />
+          </div>
 
-                    <div className="flex flex-col gap-2">
-                        <div className="flex items-center gap-2">
-                            <p className="font-bold text-2xl">
-                                {name}
-                            </p>
-                            <div className="subject-badge max-sm:hidden">
-                                {subject}
-                            </div>
-                        </div>
-                        <p className="text-lg">{topic}</p>
-                    </div>
-                </div>
-                <div className="items-start text-2xl max-md:hidden">
-                    {duration} minutes
-                </div>
-            </article>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2">
+              <p className="font-bold text-xl">{name}</p>
+              <div className="subject-badge max-sm:hidden text-xs py-0.5 px-2">{subject}</div>
+            </div>
+            <p className="text-sm">{topic}</p>
+          </div>
+        </div>
 
-            {/* <CompanionComponent
-                {...companion}
-                companionId={id}
-                userName={user.firstName!}
-                userImage={user.imageUrl!}
-            /> */}
-        </main>
-    )
-}
+        <div className="items-start text-lg max-md:hidden">{duration} minutes</div>
+      </article>
 
-export default CompanionSession
+      <CompanionComponent
+        {...companion}
+        companionId={id}
+        userName={user.firstName!}
+        userImage={user.imageUrl!}
+      />
+    </main>
+  );
+};
+
+export default CompanionSession;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { subjectsColors, voices } from "@/constants";
-// import { CreateAssistantDTO } from "@vapi-ai/web/dist/api";
+import { CreateAssistantDTO } from "@vapi-ai/web/dist/api";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -32,67 +32,51 @@ export function getSubjectColor(subject: string): [string, string] {
   }
 }
 
-// export const getSubjectColor = (subject: string) => {
-//   const subjectsColors: Record<string, string> = {
-//     maths: "#4F46E5",       // Indigo-600
-//     language: "#10B981",    // Emerald-500
-//     science: "#06B6D4",     // Cyan-500
-//     history: "#F59E0B",     // Amber-500
-//     coding: "#0EA5E9",      // Sky-500
-//     economics: "#8B5CF6",   // Violet-500
-//     finance: "#F43F5E",     // Rose-500
-//     business: "#6366F1",    // Indigo-500
-//     geography: "#22C55E",   // Green-500
-//   };
+export const configureAssistant = (voice: string, style: string) => {
+  const voiceId = voices[voice as keyof typeof voices][
+          style as keyof (typeof voices)[keyof typeof voices]
+          ] || "sarah";
 
-//   return subjectsColors[subject.toLowerCase()] || "#E5E7EB"; // fallback: Gray-200
-// };
+  const vapiAssistant: CreateAssistantDTO = {
+    name: "Companion",
+    firstMessage:
+        "Hello, let's start the session. Today we'll be talking about {{topic}}.",
+    transcriber: {
+      provider: "deepgram",
+      model: "nova-3",
+      language: "en",
+    },
+    voice: {
+      provider: "11labs",
+      voiceId: voiceId,
+      stability: 0.4,
+      similarityBoost: 0.8,
+      speed: 1,
+      style: 0.5,
+      useSpeakerBoost: true,
+    },
+    model: {
+      provider: "openai",
+      model: "gpt-4",
+      messages: [
+        {
+          role: "system",
+          content: `You are a highly knowledgeable tutor teaching a real-time voice session with a student. Your goal is to teach the student about the topic and subject.
 
-// export const configureAssistant = (voice: string, style: string) => {
-//   const voiceId = voices[voice as keyof typeof voices][
-//           style as keyof (typeof voices)[keyof typeof voices]
-//           ] || "sarah";
-
-//   const vapiAssistant: CreateAssistantDTO = {
-//     name: "Companion",
-//     firstMessage:
-//         "Hello, let's start the session. Today we'll be talking about {{topic}}.",
-//     transcriber: {
-//       provider: "deepgram",
-//       model: "nova-3",
-//       language: "en",
-//     },
-//     voice: {
-//       provider: "11labs",
-//       voiceId: voiceId,
-//       stability: 0.4,
-//       similarityBoost: 0.8,
-//       speed: 1,
-//       style: 0.5,
-//       useSpeakerBoost: true,
-//     },
-//     model: {
-//       provider: "openai",
-//       model: "gpt-4",
-//       messages: [
-//         {
-//           role: "system",
-//           content: `You are a highly knowledgeable tutor teaching a real-time voice session with a student. Your goal is to teach the student about the topic and subject.
-
-//                     Tutor Guidelines:
-//                     Stick to the given topic - {{ topic }} and subject - {{ subject }} and teach the student about it.
-//                     Keep the conversation flowing smoothly while maintaining control.
-//                     From time to time make sure that the student is following you and understands you.
-//                     Break down the topic into smaller parts and teach the student one part at a time.
-//                     Keep your style of conversation {{ style }}.
-//                     Keep your responses short, like in a real voice conversation.
-//                     Do not include any special characters in your responses - this is a voice conversation.
-//               `,
-//         },
-//       ],
-//     },
-//     clientMessages: [],
-//     serverMessages: [],
-//   };
-//   return vapiAssistant;
-// };
+                    Tutor Guidelines:
+                    Stick to the given topic - {{ topic }} and subject - {{ subject }} and teach the student about it.
+                    Keep the conversation flowing smoothly while maintaining control.
+                    From time to time make sure that the student is following you and understands you.
+                    Break down the topic into smaller parts and teach the student one part at a time.
+                    Keep your style of conversation {{ style }}.
+                    Keep your responses short, like in a real voice conversation.
+                    Do not include any special characters in your responses - this is a voice conversation.
+              `,
+        },
+      ],
+    },
+    clientMessages:[],
+    serverMessages:[]
+  };
+  return vapiAssistant;
+};
